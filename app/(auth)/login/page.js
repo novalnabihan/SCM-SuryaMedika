113166939a6a92dcc201a5f76c7d671261d051c7
noval <novalnabihan@gmail.com>
2025-05-01 23:57:06 +0800

    feat: enable functional auth login

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,9 +1,41 @@
 'use client'
 
+import { useState } from 'react';
 import { Input } from "@/app/components/ui/input"
 import { Button } from "@/app/components/ui/button"
+import { useRouter } from 'next/navigation'
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const router = useRouter()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const res = await fetch ('http://localhost:3001/api/login', {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: email,
+        password: password,
+      }),
+    })
+
+    const data = await res.json()
+
+    if (res.ok) {
+    // Menyimpan token ke localStorage untuk autentikasi berikutnya
+    localStorage.setItem('token', data.token)
+    router.push('/dashboard') // redirect ke dashboard setelah login sukses
+    } else {
+      setError(data.message)
+    }
+  }
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       {/* LEFT: Branding */}
@@ -25,7 +57,7 @@ export default function LoginPage() {
         <div className="w-full max-w-md rounded-3xl bg-white p-10 shadow-sm">
           <h2 className="text-2xl font-bold mb-10 text-center">Selamat Datang Kembali!</h2>
           
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div className="space-y-2">
               <label htmlFor="email" className="text-sm font-medium text-gray-700">E-mail</label>
               <Input 
@@ -33,6 +65,8 @@ export default function LoginPage() {
                 type="email" 
                 placeholder="Masukkan email Anda" 
                 className="h-14 text-base px-4 border border-gray-200 rounded-xl focus:ring-1 focus:ring-cyan-600 focus:border-cyan-600"
+                value = {email}
+                onChange ={(e) => setEmail(e.target.value)}
               />
             </div>
             
@@ -43,9 +77,13 @@ export default function LoginPage() {
                 type="password" 
                 placeholder="Masukkan password" 
                 className="h-14 text-base px-4 border border-gray-200 rounded-xl focus:ring-1 focus:ring-cyan-600 focus:border-cyan-600"
+                value = {password}
+                onChange = {(e) => setPassword(e.target.value)}
               />
             </div>
             
+            {error && <p className='text-red-400 text-center'>{error}</p>}
+
             <Button className="w-full h-14 text-base font-medium bg-cyan-700 hover:bg-cyan-800 rounded-xl mt-6 py-4">
               Login
             </Button>
