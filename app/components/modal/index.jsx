ec7ccfa3f7a9e2980ae51a034b42af25d66c5c12
wahyuhidayat <muhammadwahyuhidayatmm1@gmail.com>
2025-05-01 21:48:11 +0800

    feat: add customModal component

---
+++ app/components/modal/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import {
+    Dialog,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@/app/components/ui/dialog";
+import { Button } from "../ui/button";
+
+const CustomModal = ({
+    title, children, textButton, open, setOpen, icon
+}) => {
+    return <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+            <Button className="flex items-center gap-2 bg-cyan-950 hover:bg-cyan-900 text-white px-5 py-6 rounded-lg min-w-[200px] text-base">
+                {icon}
+                {textButton}
+            </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-4xl p-6 bg-white rounded-xl shadow-lg">
+            <DialogHeader>
+                <DialogTitle className="text-xl font-semibold">
+                    {title}
+                </DialogTitle>
+            </DialogHeader>
+            {children}
+        </DialogContent>
+    </Dialog>
+}
+
+
+export default CustomModal
