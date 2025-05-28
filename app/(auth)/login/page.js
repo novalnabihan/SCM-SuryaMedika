efb533bd2b3a8ca4e8544e1c85c7b49d8c2e207e
wahyuhidayat <muhammadwahyuhidayatmm1@gmail.com>
2025-05-28 18:29:49 +0800

    feat: improve user feedback for invalid credentials on login

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -82,7 +82,12 @@ export default function LoginPage() {
               />
             </div>
             
-            {error && <p className='text-red-400 text-center'>{error}</p>}
+                      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Email atau password salah</strong>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/></svg>
+            </span>
+          </div>
 
             <Button className="w-full h-14 text-base font-medium bg-cyan-700 hover:bg-cyan-800 rounded-xl mt-6 py-4">
               Login
